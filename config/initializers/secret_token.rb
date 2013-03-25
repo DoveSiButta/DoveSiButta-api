0b63bc5c89d8c5e46fd0c321eced0000b3f5d5dd
Giovanni Maggini <giovanni.maggini@gmail.com>
2013-03-26 00:21:37 +0100

    First commit. Created app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dovesibutta::Application.config.secret_token = '9499c01b7b7a1975eb98bf07bfc5889bc6b5a142b6cb499651a1f9ef3ddeb7fee65b9659853d6071d7d4613f255bcc552529d376d61cac60351c7e0e82fa5504'
